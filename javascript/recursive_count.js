0f21372af2f6920e220e3da7000cf2dc86297991
Daniel <daniel.arisa@yahoo.com>
2023-01-24 23:29:40 -0600

    first commit

---
+++ javascript/recursive_count.js
@@ -0,0 +1,17 @@
+function recursiveCount(num = 0) {
+  if (num === 10) {
+    return;
+  }
+  console.log(num);
+  recursiveCount(num + 1);
+}
+
+if (require.main === module) {
+  recursiveCount();
+}
+
+module.exports = recursiveCount;
+
+// OPTIONAL
+// Please add your pseudocode to this file
+// And a written explanation of your solution
